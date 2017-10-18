7c07af1a25652a9781c6566a8bc08a014afbc610
ZhuLifeng <820917549@qq.com>
2017-10-19 01:10:55 +0800

    118. Pascal's Triangle

---
+++ 118. Pascal's Triangle.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res;
+		for (int i = 0; i < numRows; i++) {
+			vector<int> row;
+			if (i == 0 || i == 1) {
+				for (int j = 0; j < i + 1; j++) {
+					row.push_back(1);
+				}
+			} else {
+				for (int j = 0; j < i + 1; j++) {
+					if (j == 0 || j == i) row.push_back(1);
+					else row.push_back(res[i - 1][j - 1] + res[i - 1][j]);
+				}
+			}
+			res.push_back(row);
+		} 
+    }
+};
