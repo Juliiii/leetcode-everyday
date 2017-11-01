ddcafc5caac8f320e2fb1fc4c6bc24a709dab728
ZhuLifeng <820917549@qq.com>
2017-11-01 06:17:02 -0500

    374. Guess Number Higher or Lower

---
+++ 374. Guess Number Higher or Lower.cpp
@@ -0,0 +1,20 @@
+// Forward declaration of guess API.
+// @param num, your guess
+// @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+int guess(int num);
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int left = 1, right = n;
+        while(left <= right) {
+            int mid = (right - left) / 2 + left;
+            if (guess(mid) == -1) {
+                right = mid - 1;
+            } else if (guess(mid) == 1) {
+                left = mid + 1;
+            } else return mid;
+        }
+        return -1;
+    }
+};
