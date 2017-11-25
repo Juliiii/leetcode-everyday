21df5434bf416da5d18a43110c8a141d44acd2a3
ZhuLifeng <820917549@qq.com>
2017-11-25 18:34:13 +0800

    199. Binary Tree Right Side View

---
+++ medium/199. Binary Tree Right Side View.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        if (!root) return res;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()) {
+            int n = q.size();
+            TreeNode* cur;
+            for (int i = 0; i < n; i++) {
+                cur = q.front();
+                if (i == n - 1) {
+                    res.push_back(cur->val);
+                }
+                q.pop();
+                if (cur->left) q.push(cur->left);
+                if (cur->right) q.push(cur->right);
+            }
+        }
+        return res;
+    }
+};
